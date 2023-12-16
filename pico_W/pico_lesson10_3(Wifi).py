eb62c7cc8782168cec56084cf43b5bbd2fa5e6a9
Hsuwairan <151415013+Hsuwairan@users.noreply.github.com>
2023-12-16 16:02:51 +0800

    modify

---
+++ pico_W/pico_lesson10_3(Wifi).py
@@ -0,0 +1,30 @@
+import network
+import time
+
+ssid = 'Galaxy S21 Ultra 5G'
+password = '29834762'
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(ssid,password)
+
+#等待連線失敗
+#status=0,1,2正在連線
+#status=3連線成功
+#<0,>3失敗連線
+
+max_walt = 10
+while max_walt > 0:
+    status = wlan.status()
+    if status < 0 or status >= 3:
+        break
+    max_walt -= 2
+    print("等待連線")
+    time.sleep(1)
+    
+if wlan.status() != 3:
+    raise RuntimeError("連線失敗")
+else:
+    print('連線成功')
+    configure = wlan.ifconfig()
+    print(f'IP{configure[0]}')
