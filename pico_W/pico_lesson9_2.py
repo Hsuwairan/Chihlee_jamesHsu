98f1af56afe7f6421eae36c4dfe21ef28539c7e2
Hsuwairan <151415013+Hsuwairan@users.noreply.github.com>
2023-12-16 11:58:18 +0800

    modify

--- pico_W/pico_lesson9_2.py
+++ pico_W/pico_lesson9_2.py
@@ -2,6 +2,10 @@ from machine import Pin
 from machine import Timer
 
 def mycallback1(t):
-    print(type(t))
+    print(1)
+def mycallback2(t):
+    print(2)    
 time1 = Timer()
-time1.init(freq=1,callback=mycallback1)
+time1.init(freq=1,period=1000,callback=mycallback1)
+time2 = Timer()
+time2.init(freq=1,period=2000,callback=mycallback2)
