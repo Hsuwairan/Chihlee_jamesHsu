f6b108c41a7c9b856758624a93a39525026add73
Hsuwairan <151415013+Hsuwairan@users.noreply.github.com>
2024-01-20 14:07:36 +0800

    modify

---
+++ pico_W/pico_lesson13_1.py
@@ -0,0 +1,20 @@
+from machine import Pin
+import time
+
+red_led = Pin(15,mode=Pin.OUT)
+btn = Pin(14,mode=Pin.PULL_DOWN)
+is_press = False
+led_status = False
+
+while True:
+    if btn.value():
+        time.sleep_ms(50)
+        if btn.value():
+            is_press = True
+    elif is_press:
+        time.sleep_ms(50)
+        if btn.value == False:
+            print('release')
+            led_status = not led_status
+            red_led.value(led.stauus)
+            is_press = False
